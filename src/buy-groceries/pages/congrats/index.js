126eb8602de00c6cc7dc75f70c02e33457c5d8e7
Noman Aamir <noman.aamir@yahoo.com>
2021-05-15 17:21:00 +0500

    disabled back handler button and fix UI issue

--- src/buy-groceries/pages/congrats/index.js
+++ src/buy-groceries/pages/congrats/index.js
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'white',
         justifyContent: 'center',
-        alignContent: 'center'
+        alignItems: 'center'
     }
 });
 
